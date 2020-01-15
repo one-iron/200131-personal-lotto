e4326c5de1d9334346e80160e9ee3fd44de55318
one-iron <59141662+one-iron@users.noreply.github.com>
2020-01-15 17:00:11 +0900

    Add files via upload

--- 20200115.js
+++ 20200115.js
@@ -5,14 +5,33 @@ function _1headmenu(n) {
   for(i = 1; i <= a_sel.getElementsByTagName('a').length; i++){
     document.getElementById('_1headseconditem'+i).className = "item";
   }
-  for(i =0; i <= u_sel.getElementsByClassName('ui segment').length-1; i++){
+  for(i = 0; i <= u_sel.getElementsByClassName('ui segment').length-1; i++){
     document.getElementById('_1bodybox'+i).style.display = "none";
   }
-  if(n !== 0){
+
+
+
+  if(n !== 0 && n !== 4){
     document.getElementById('_1headseconditem'+n).className = "active item";
     document.getElementById('_1bodybox'+n).style.display = "inline-block";
+    document.getElementById('_1foot1').style.display = "inline-block";
+    document.getElementById('_1foot2').style.display = "none";
+    document.getElementById('_1headfirstbutt2').style.display = "none";
+    document.getElementById('_1headfirstbutt').style.display = "inline-block";
   }
   else if (n == 0) {
     document.getElementById('_1bodybox'+n).style.display = "inline-block";
+    document.getElementById('_1foot2').style.display = "_1foot1";
+    document.getElementById('_1foot1').style.display = "inline-block";
+    document.getElementById('_1foot2').style.display = "none";
+    document.getElementById('_1headfirstbutt2').style.display = "none";
+    document.getElementById('_1headfirstbutt').style.display = "inline-block";
+  }
+  else if (n == 4) {
+    document.getElementById('_1bodybox'+n).style.display = "inline-block";
+    document.getElementById('_1foot1').style.display = "none";
+    document.getElementById('_1foot2').style.display = "inline-block";
+    document.getElementById('_1headfirstbutt').style.display = "none";
+    document.getElementById('_1headfirstbutt2').style.display = "inline-block";
   }
 }
