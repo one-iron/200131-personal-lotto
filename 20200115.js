bc9eb67facd12f157c2d097d38f1e8fefd0a57f2
one-iron <59141662+one-iron@users.noreply.github.com>
2020-01-18 14:44:25 +0900

    Add files via upload

--- 20200115.js
+++ 20200115.js
@@ -1,3 +1,7 @@
+var result1 = [];
+var random6num = [];
+var winnums = [1,15,17,23,25,41];
+var color = ["red","orange","olive","green","brown","violet","yellow","pink","purple","blue"]
 
 function _1headmenu(n) {
   var a_sel = document.querySelector(".ui.secondary.pointing.menu");
@@ -36,16 +40,11 @@ function _1headmenu(n) {
     document.getElementById('_1headfirstbutt2').style.display = "inline-block";
   }
 }
-
-var result1 = [];
-var color = ["red","orange","olive","green","brown","violet","yellow","pink","purple","blue"]
-
-
 function out() {
   result1.unshift([]);
   for(i = 0; result1[0].length <= 5; i++){
     var num = Math.floor(Math.random() * 45 + 1);
-    if(result1[0].indexOf(num) == -1){
+    if(result1[0].indexOf(num) == -1 && random6num.join().indexOf(num) == -1){
       result1[0].push(num);
     }
   }
@@ -55,6 +54,54 @@ function out() {
   li.appendChild(no);
   var cr = Math.floor(Math.random()*10)
   document.getElementById('result').appendChild(li).className = "ui inverted "+color[cr]+" button";
+}
+function dele() {
+  var va = document.getElementById("selectmenu").value;
+  if(va == "0"){
+    random6()
+  }
+  else if (va == "1") {
+    winnum();
+  }
+}
+
+function random6() {
+  var arr = [];
+  for(i = 0; arr.length <= 5; i++){
+    var randomnum = Math.floor(Math.random() * 45 + 1);
+    if(arr.indexOf(randomnum) == -1){
+      arr.push(randomnum);
+      document.getElementById('n'+ randomnum).className = "ui basic button";
+      document.getElementById('n'+ randomnum).style.textDecoration = "line-through red";
+    }
+    random6num.push(arr);
+  }
+  var li = document.createElement("button");
+  var no = document.createTextNode(Math.floor(random6num.length/6)+ "번째 랜덤 번호 : "+arr.sort(function(a,b){return a-b;}))
+  li.appendChild(no);
+  document.getElementById('listnums').appendChild(li).className = "ui button";
+}
+function winnum(){
+  for(i = 0; i <= 5; i++){
+    document.getElementById('n'+ winnums[i]).className = "ui basic button";
+    document.getElementById('n'+ winnums[i]).style.textDecoration = "line-through red";
+  }
+  random6num.push("["+winnums+"]");
+  var li = document.createElement("button");
+  var no = document.createTextNode("최근 당첨 번호 : "+ winnums);
+  li.appendChild(no);
+  document.getElementById('listnums').appendChild(li).className = "ui button";
+}
+function reset1(){
+  random6num.splice(0,random6num.length);
+  for(i = 1; i <= 45; i++){
+    document.getElementById('n'+ i).className="ui circular facebook icon button";
+    document.getElementById('n'+ i).style.textDecoration = "none";
+  }
+  var list = document.getElementById("listnums");
+  while (list.hasChildNodes()) {
+    list.removeChild(list.firstChild);
+  }
 }
 
 function reset2() {
